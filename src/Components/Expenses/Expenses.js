874f55acece61acfe78d70221d8edcbde51f1dce
Hasan <hasanalaniIT@users.noreply.github.com>
2021-08-12 13:38:30 +0300

    Adding Expense filter with css and using map for array

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,10 +1,28 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import React, { useState } from "react";
+import ExpensesFilter from "../NewExpense/ExpensesFilter";
 const Expenses = (props) => {
+  const [filterdYear, setFilterdYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilterdYear(selectedYear);
+  };
   return (
     <Card className="expenses">
-      <ExpenseItem
+      <ExpensesFilter
+        selected={filterdYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+      {/* <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -23,7 +41,7 @@ const Expenses = (props) => {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      ></ExpenseItem>
+      ></ExpenseItem> */}
     </Card>
   );
 };
