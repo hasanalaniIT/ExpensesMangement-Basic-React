24597bfe4da5ee63eeb65eae32e25c68d7982f47
Hasan <hasanalaniIT@users.noreply.github.com>
2021-08-13 18:20:58 +0300

    Adding Expenses List and its CSS file

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import React, { useState } from "react";
 import ExpensesFilter from "../NewExpense/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 const Expenses = (props) => {
   const [filterdYear, setFilterdYear] = useState("2021");
 
@@ -13,18 +13,7 @@ const Expenses = (props) => {
   const filterdExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filterdYear;
   });
-  let expenseContent = <p>No Expenses Found!!</p>;
 
-  if (filterdExpenses.length > 0) {
-    expenseContent = filterdExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
   return (
     <div>
       <Card className="expenses">
@@ -32,7 +21,7 @@ const Expenses = (props) => {
           selected={filterdYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expenseContent}
+        <ExpensesList items={filterdExpenses} />
       </Card>
     </div>
   );
