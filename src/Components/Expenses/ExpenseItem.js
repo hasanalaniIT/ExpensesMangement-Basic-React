5471939e5d79af968170f58b84bf8d144d652d5f
hasanalaniit <redfire5005@gmail.com>
2021-08-14 21:14:50 +0300

    Finshing the first react project with udemy

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
   console.log("ExpenseItem evaluated by React");
-
-  const clickHandler = () => {
-    setTitle("Updated");
-    console.log(title);
-  };
+  
+  // const [title, setTitle] = useState(props.title);
+  // const clickHandler = () => {
+  //   setTitle("Updated");
+  //   console.log(title);
+  // };
 
   return (
     <li>
@@ -19,7 +19,6 @@ const ExpenseItem = (props) => {
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
       </Card>
     </li>
   );
